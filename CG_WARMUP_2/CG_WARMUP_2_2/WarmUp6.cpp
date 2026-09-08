b1b3f1559d56d692fff51872b9bbf6fee7a5ff59
jogomme <beomgih945@gmail.com>
2026-09-08 22:09:36 +0900

    2단계 - Vertex, VT, Face 구조체 및 데이터 저장 함수 추가
    ss를 처음 사용해보았습니다.
    
    ss(line)으로 한 줄을 통채로 읽어와서 보는게 굉장히 편한 느낌을 받았습니다.
    
    Vertex 구조체 추가
    VT 구조체 추가
    Face 구조체 추가
    SaveVertex() 구현
    SaveVT() 구현
    SaveF() 1차 구현

--- CG_WARMUP_2/CG_WARMUP_2_2/WarmUp6.cpp
+++ CG_WARMUP_2/CG_WARMUP_2_2/WarmUp6.cpp
@@ -1,8 +1,44 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
+struct Vertex
+{
+    float x;
+    float y;
+    float z;
+};
+
+struct VT
+{
+    float x;
+    float y;
+};
+
+struct Face
+{
+    int vertexIndex[3];
+    int textureIndex[3];
+    bool isTexture;
+};
+    
+Vertex vertex[100];
+int vertexCount{};
+
+VT vt[100];
+int vtCount{};
+
+Face face[100];
+int faceCount{};
+
+void SaveVertex(std::string line);
+void SaveVT(std::string line);
+void SaveF(std::string line);
+
+//-------------------------------------------------------------------------------------------------
 int main()
+//-------------------------------------------------------------------------------------------------
 {
     std::ifstream file("data1.txt");
 
@@ -16,27 +52,71 @@ int main()
 
     while (std::getline(file, line))
     {
-        while (std::getline(file, line))
+        if (line[0] == '#')
         {
-            if (line[0] == '#')
-            {
-                std::cout << "주석입니다.\n";
-            }
-            else if (line[0] == 'v' && line[1] == 't')
-            {
-                std::cout << "텍스처 좌표입니다.\n";
-            }
-            else if (line[0] == 'v')
-            {
-                std::cout << "정점 좌표입니다.\n";
-            }
-            else if (line[0] == 'f')
-            {
-                std::cout << "삼각형입니다.\n";
-            }
+            std::cout << "주석입니다.\n";
         }
-        std::cout << line << '\n';
+        else if (line[0] == 'v' && line[1] == 't')
+        {
+            std::cout << "텍스처 좌표입니다.\n";
+            SaveVT(line);
+        }
+        else if (line[0] == 'v')
+        {
+            std::cout << "정점 좌표입니다.\n";
+            SaveVertex(line);
+        }
+        else if (line[0] == 'f')
+        {
+            std::cout << "삼각형입니다.\n";
+            SaveF(line);
+        }
     }
 
     file.close();
+}
+
+//-------------------------------------------------------------------------------------------------
+void SaveVertex(std::string line)
+//-------------------------------------------------------------------------------------------------
+{
+    std::stringstream ss(line);
+
+    char type;
+
+    ss >> type >> vertex[vertexCount].x >> vertex[vertexCount].y >> vertex[vertexCount++].z;
+}
+
+//-------------------------------------------------------------------------------------------------
+void SaveVT(std::string line)
+//-------------------------------------------------------------------------------------------------
+{
+    std::stringstream ss(line);
+
+    std::string type;
+
+    ss >> type >> vt[vtCount].x >> vt[vtCount++].y;
+}
+
+//-------------------------------------------------------------------------------------------------
+void SaveF(std::string line)
+//-------------------------------------------------------------------------------------------------
+{
+    for (int i = 0; i < line.length(); ++i) {
+        if (line[i] == '/') {
+            line[i] = ' ';
+        }
+    }
+
+    std::stringstream ss(line);
+
+    char type;
+
+    ss >> type 
+        >> face[faceCount].vertexIndex[0] >> face[faceCount].textureIndex[0]
+        >> face[faceCount].vertexIndex[1] >> face[faceCount].textureIndex[1]
+        >> face[faceCount].vertexIndex[2] >> face[faceCount].textureIndex[2];
+
+    face[faceCount++].isTexture = true;
+
 }
