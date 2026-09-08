2ac74d5ca87fb7bf8c9ef5cfcf7ad380f7dabb72
jogomme <beomgih945@gmail.com>
2026-09-08 21:44:24 +0900

    1단계 - data 파일 추가 및 파일 읽기가 잘 되는지 확인

---
+++ CG_WARMUP_2/CG_WARMUP_2_2/WarmUp6.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    std::ifstream file("data1.txt");
+
+    if (!file)
+    {
+        std::cout << "파일을 열 수 없습니다.\n";
+        return 2025180028;
+    }
+
+    std::string line;
+
+    while (std::getline(file, line))
+    {
+        while (std::getline(file, line))
+        {
+            if (line[0] == '#')
+            {
+                std::cout << "주석입니다.\n";
+            }
+            else if (line[0] == 'v' && line[1] == 't')
+            {
+                std::cout << "텍스처 좌표입니다.\n";
+            }
+            else if (line[0] == 'v')
+            {
+                std::cout << "정점 좌표입니다.\n";
+            }
+            else if (line[0] == 'f')
+            {
+                std::cout << "삼각형입니다.\n";
+            }
+        }
+        std::cout << line << '\n';
+    }
+
+    file.close();
+}
